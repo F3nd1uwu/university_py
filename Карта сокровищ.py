503fd311b7351c380559ab8fb65391e031fa61c0
F3nd1uwu <fendi.xxd@gmail.com>
2024-04-03 14:19:03 +0400

    123456

--- Карта сокровищ.py
+++ Карта сокровищ.py
@@ -1,5 +1,10 @@
 n = int(input())
-tresures = [tuple(map(int, input().split()) for _ in range (n))]
+tresures = [tuple(map(int, input().split())) for _ in range (n)]
 tresure_group = {}
 
+for x in tresures:
+    for x_i in x:
+        print(x_i)
+    
 
+print(tresure_group)
